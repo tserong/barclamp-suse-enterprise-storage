a9c46f9f2122fecd150b6c13f5ffcf9a50fb93df
Tim Serong <tserong@suse.com>
2014-12-15 18:16:25 +1100

    Add stub keystone cookbook
    Signed-off-by: Tim Serong <tserong@suse.com>

---
+++ chef/cookbooks/keystone/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Dell, Inc."
+maintainer_email "crowbar@Dell.com"
+license          "Apache 2.0 License, Copyright (c) 2011 Dell Inc. - http://www.apache.org/licenses/LICENSE-2.0"
+description      "Stub Keystone server deployment recipes to satisfy ceph barclamp requirements."
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "1.0"
